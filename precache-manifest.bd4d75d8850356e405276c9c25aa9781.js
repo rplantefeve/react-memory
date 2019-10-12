eacb6797b58530b22e15fcbabcd4d6b6b11a1937
rplantefeve <romain.plantefeve@gmail.com>
2019-10-12 14:26:10 +0200

    Updates

---
+++ precache-manifest.bd4d75d8850356e405276c9c25aa9781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f382827eeb7d7deedda03094192d8a7",
+    "url": "/react-memory/index.html"
+  },
+  {
+    "revision": "db86cade9c872382ad42",
+    "url": "/react-memory/static/css/main.c5e4e079.chunk.css"
+  },
+  {
+    "revision": "cd13299aa427b445771c",
+    "url": "/react-memory/static/js/2.4bea5fa3.chunk.js"
+  },
+  {
+    "revision": "db86cade9c872382ad42",
+    "url": "/react-memory/static/js/main.c7274b94.chunk.js"
+  },
+  {
+    "revision": "ba81e59c9abed793e3f9",
+    "url": "/react-memory/static/js/runtime-main.1bb98255.js"
+  },
+  {
+    "revision": "d4e439c9f5f34a044bf85745f3f117f1",
+    "url": "/react-memory/static/media/cards.d4e439c9.png"
+  },
+  {
+    "revision": "1fb0169964ba2b3eee78329da1b6e249",
+    "url": "/react-memory/static/media/happy_face.1fb01699.png"
+  }
+]);
